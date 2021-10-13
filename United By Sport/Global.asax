790e570c0efe26b768e01dbf512ffe0a7bed0617
DominikChabera <dominikchabera@gmail.com>
2021-10-13 20:12:23 +0200

    Initial commit

---
+++ United By Sport/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="United_By_Sport.MvcApplication" Language="C#" %>
---
+++ United By Sport/Models/User.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace United_By_Sport.Models
+{
+    public class User
+    {
+        [Key, Column(Order = 1)]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int idUser { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Minimum 3 char, Max 50 char")]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Minimum 3 char, Max 50 char")]
+        public string LastName { get; set; }
+        [Required]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Email adres contains illegal characters!!")]
+        public string Email { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,15}$", ErrorMessage = "Password contains illegal characters!!")]
+
+        public string Password { get; set; }
+
+        [NotMapped]
+        [Required]
+        [System.ComponentModel.DataAnnotations.Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [StringLength(31,ErrorMessage = "To Long phone number max 31 char")]
+        public string PhoneNumber { get; set; }
+
+
+        public string FullName()
+        {
+            return this.FirstName + " " + this.LastName;
+        }
+
+        public string GetPhoneNumber()
+        {
+            return this.PhoneNumber;
+        }
+        public string GetEmailAddres()
+        {
+            return this.Email;
+        }
+    }
+}
